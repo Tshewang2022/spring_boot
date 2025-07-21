09ca4c54a26b1a298a93a370b48352d24e8f4626
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2025-07-21 16:28:01 +0600

    updated the code

---
+++ kafka_prod_cons/kafkaconsumer/src/test/java/tshewang/kafkaconsumer/KafkaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package tshewang.kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
